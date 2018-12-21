0080a6f7214a919a03a03099a3e4cd890f5db52a
Roberto Arruda Braga <rob.arr.bra@gmail.com>
2018-12-21 13:29:46 -0300

    adicionada funcao que busca pelo elemento com mensos interacoes

--- ex12.h
+++ ex12.h
@@ -69,32 +69,32 @@ void coleta_transicao(ltrans_t **list, FILE *stream)
         slei=strok(NULL, " ");
         sef=strok(NULL, " ");
         insere_transicao(list, atoi(sei), slei, atoi(sef);
-    }
-}
+                }
+                }
 
-void estados_limites(quintupla_t *Q)
+                void estados_limites(quintupla_t *Q)
 
-{
+                {
 
-    lest_t *pl= Q->F;
+                lest_t *pl= Q->F;
 
-    insere_transicao(&Q->D, Q->K, "E", Q->S);
+                insere_transicao(&Q->D, Q->K, "E", Q->S);
 
 
 
-    while
+                while
 
-    {   
+                {   
 
-        insere_transicao(&Q->D, pl->estado, "E", Q->K+1);
+                insere_transicao(&Q->D, pl->estado, "E", Q->K+1);
 
-        pl= pl->prox;
+                pl= pl->prox;
 
-     }
+                }
 
-    return;
+                return;
 
-    }
+                }
 
 int lista_unitaria(ltrans_t *list)
 {
@@ -104,3 +104,41 @@ int lista_unitaria(ltrans_t *list)
 }
 
 
+
+
+void uniao(ltrans_t **list)
+{
+    ltrans_t *pl, *pl2, res/* variavel que vai unificar as transicoes semelhantes*/;
+
+    while(busca_semelhantes(*list, &pl, &pl2)) /* procurando por transicoes semelhantes, retorna 0 se nada for encontrado, e 1 se encontrado*/
+    {
+        res.ei= pl->ei;
+        res.ef= pl->ef;
+        res.lei= malloc(definir_tamanho(pl->lei, pl2->lei) * sizeof(char)); /* definindo o tamanho da string que vai unir as leis*/
+
+        /* processo de atribuicao da uniao das leis em um string*/
+        if(strcmp(pl->lei, "E") != 0)
+        {
+            strcpy(res.lei, pl->lei);
+            if(strcmp(pl2->lei, "E") != 0)
+            {
+                strcat(res.lei, "|");
+                strcat(res.lei, pl2->lei);
+            }
+        }
+        else
+        {
+            if(strcmp(pl2->lei, "E") != 0)
+                strcpy(res.lei, pl2->lei);
+            else
+                strcpy(res.lei, "E");
+        }
+        /* ------------- */
+
+        remove_transicao(list, pl);
+        remove_transicao(list, pl2);
+        insere_transicao(list, res.ei, res.lei, res.ef);
+        free(res.lei);
+    }
+    return;
+}
